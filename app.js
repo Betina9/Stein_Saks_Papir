b78780beefb01992c281723a077b74321f8e6dd1
Betina9 <ob042025@gmail.com>
2025-10-17 11:20:23 +0200

    la inn hender og bilder

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-let hands = ["rock", "paper", "scissors"];
+const hands = ["rock", "paper", "scissors"];
+const handAnimation = document.getElementById("handAnimation");
 
 function getHand() {
-  let randomIndex = Math.floor(Math.random() * 3);
+  const randomIndex = Math.floor(Math.random() * 3);
   return hands[randomIndex];
 }
+
+/* Håndanimasjonen*/
+function playHandAnimation() {
+  handAnimation.currentTime = 0;
+  handAnimation.play();
+
+  setTimeout(() => {
+    handAnimation.pause();
+  }, 5000);
+}
+
+playHandAnimation();
