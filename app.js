04f91d7f893fa1320a8b5c3859166625e4879dd2
Betina9 <ob042025@gmail.com>
2025-10-17 13:45:27 +0200

    Fått inn functioner som får spillet til å fungere

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
 const hands = ["rock", "paper", "scissors"];
 const handAnimation = document.getElementById("handAnimation");
+const userChoice = document.getElementById("userChoice");
+const resultText = document.getElementById("result");
 
+/* Spilleren velger en hånd*/
 function getHand() {
   const randomIndex = Math.floor(Math.random() * 3);
   return hands[randomIndex];
 }
 
 /* Håndanimasjonen*/
-function playHandAnimation() {
+function play(playerChoice) {
+  resultText.textContent = "";
   handAnimation.currentTime = 0;
+  handAnimation.style.display = "block";
   handAnimation.play();
 
+  /* Etter animasjonen*/
   setTimeout(() => {
     handAnimation.pause();
-  }, 5000);
-}
+    handAnimation.style.display = "none";
 
-playHandAnimation();
+    const computerChoice = getHand();
+    let result;
+
+    if (playerChoice === computerChoice) {
+      result = "Uavgjort!";
+    } else if (
+      (playerChoice === "rock" && computerChoice === "scissors") ||
+      (playerChoice === "paper" && computerChoice === "rock") ||
+      (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
+      result = "Du vant!🥳✨";
+    } else {
+      result = "Datamaskinen vant!😞";
+    }
+    resultText.textContent = `Datamaskinens valg: ${computerChoice}. ${result}`;
+  }, 2000);
+}
